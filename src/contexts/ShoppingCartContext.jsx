8e168ac6a47e44be31fdcefaa6b0cfe809a128b5
jdhtoro <juandhtoro@gmail.com>
2024-04-02 00:54:36 -0500

    cuarto commit

--- src/contexts/ShoppingCartContext.jsx
+++ src/contexts/ShoppingCartContext.jsx
@@ -1,115 +1,3 @@
-// import { createContext, useEffect, useState } from "react";
-// import useLocalStorage from "../hooks/useLocalStorage";
-// import PropTypes from "prop-types";
-
-// const ShoppingCartContext = createContext();
-
-// const ShoppingCartProvider = (props) => {
-//     const { children } = props;
-//     const { items, setItem } = useLocalStorage({ shoppingCart: [], products: [] });
-//     const [ shoppingCart, setShoppingCart ] = useState(items.shoppingCart || []);
-//     const [ products, setProducts ] = useState(items.products || []);
-//     const [ purchaseSuccess, setPurchaseSuccess ] = useState(false);
-
-//     useEffect(() => {
-//         setShoppingCart(items.shoppingCart || []);
-//     }, [items.shoppingCart]);
-
-//     useEffect(() => {
-//         setProducts(items.products || []);
-//     }, [items.products]);
-
-//     const getProductCart = (id) => {
-//         return shoppingCart.find((item) => item.id === id);
-//     };
-
-//     const addProductCart = (product) => {
-//         setShoppingCart((prevCart) => {
-//             const productQueEstaEnLS = getProductCart(product.id);
-//             const productEnStock = products.find((item) => item.id === product.id);
-
-//             if (productQueEstaEnLS && productEnStock && productEnStock.stock > productQueEstaEnLS.amount) {
-//                 const updatedCart = prevCart.map((item) =>
-//                     item.id === product.id ? { ...item, amount: item.amount + 1 } : item,
-//                 );
-//                 setItem("shoppingCart", updatedCart);
-//                 return updatedCart;
-//             } else if (!productQueEstaEnLS && productEnStock && productEnStock.stock > 0) {
-//                 const updatedCart = [ ...prevCart, { ...product, amount: 1 }];
-//                 setItem("shoppingCart", updatedCart);
-//                 return updatedCart;
-//             } else {
-//                 console.log("No hay suficiente stock disponible.");
-//                 return prevCart;
-//             }
-//         });
-//     };
-
-//     const decreaseProductCart = (productId) => {
-//         setShoppingCart((prevCart) =>
-//             prevCart.map((item) =>
-//                 item.id === productId ? { ...item, amount: Math.max(0, item.amount - 1) } : item,
-//             ),
-//         );
-//     };
-
-//     const removeProductFromCart = (productId) => {
-//         setShoppingCart((prevCart) => prevCart.filter((item) => item.id !== productId));
-//     };
-
-//     const updateProductInCart = (updatedProduct) => {
-//         setShoppingCart((prevCart) =>
-//             prevCart.map((item) => (item.id === updatedProduct.id ? { ...item, ...updatedProduct } : item)),
-//         );
-//     };
-
-//     const clearCart = () => {
-//         setShoppingCart([]);
-//     };
-
-//     const processPurchase = () => {
-//         const updatedProducts = products.map((product) => {
-//             const cartItem = getProductCart(product.id);
-//             if (cartItem) {
-//                 return { ...product, stock: product.stock - cartItem.amount };
-//             }
-//             return product;
-//         });
-//         setProducts(updatedProducts);
-//         setItem("products", updatedProducts);
-//         clearCart();
-//         setPurchaseSuccess(true);
-//         setTimeout(() => {
-//             setPurchaseSuccess(false);
-//             window.location.reload();
-//         }, 3000);
-//     };
-
-//     return (
-//         <ShoppingCartContext.Provider
-//             value={{
-//                 shoppingCart,
-//                 getProductCart,
-//                 addProductCart,
-//                 decreaseProductCart,
-//                 removeProductFromCart,
-//                 updateProductInCart,
-//                 clearCart,
-//                 processPurchase,
-//                 purchaseSuccess,
-//             }}
-//         >
-//             {children}
-//         </ShoppingCartContext.Provider>
-//     );
-// };
-
-// ShoppingCartProvider.propTypes = {
-//     children: PropTypes.node.isRequired,
-// };
-
-// export { ShoppingCartContext, ShoppingCartProvider };
-
 import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
@@ -169,9 +57,7 @@ const ShoppingCartProvider = (props) => {
                     const productResponse = await axios.get(
                         `https://mitienda-juan.onrender.com/api/products/${cartItem.id}`,
                     );
-                    const product = productResponse.data; // Obtener los datos del producto desde la respuesta
-
-                    // Verificar si product.stock es un número válido y cartItem.amount es un número válido
+                    const product = productResponse.data;
                     if (!isNaN(product.stock) && !isNaN(cartItem.amount)) {
                         const updatedStock = product.stock - cartItem.amount;
                         await axios.put(
@@ -184,16 +70,12 @@ const ShoppingCartProvider = (props) => {
                             product.stock,
                             cartItem.amount,
                         );
-                        // Manejar el error aquí si es necesario
                     }
                 }),
             );
             setShoppingCart([]);
-            // Aquí podrías establecer un estado para indicar que la compra fue exitosa si es necesario
-            // También podrías redirigir al usuario a una página de confirmación de compra
         } catch (error) {
             console.error("Error processing purchase:", error);
-            // Manejar el error aquí si es necesario
         }
     };
 
@@ -206,7 +88,7 @@ const ShoppingCartProvider = (props) => {
                 decreaseProductCart,
                 removeProductFromCart,
                 processPurchase,
-                clearCart, // Incluido clearCart en el contexto
+                clearCart,
             }}
         >
             {children}
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,99 +1,9 @@
-// import useLocalStorage from "./useLocalStorage.js";
-// import { useContext } from "react";
-// import { ShoppingCartContext } from "../contexts/ShoppingCartContext";
-
-// import { pizzas } from "../data/data.js";
-
-// const useProducts = () => {
-//     const { items, setItem } = useLocalStorage({ products: pizzas });
-//     const { removeProductFromCart } = useContext(ShoppingCartContext);
-
-//     const normalizeValue = (value = "") => {
-//         return value
-//             .toLowerCase()
-//             .trim()
-//             .replace("á", "a")
-//             .replace("é", "e")
-//             .replace("í", "i")
-//             .replace("ó", "o")
-//             .replace("ú", "u");
-//     };
-
-//     const searchProducts = (text) => {
-//         const preparedText = normalizeValue(text);
-
-//         return items.products.filter((pizza) => {
-//             const preparedPizza = normalizeValue(pizza.name);
-
-//             if (preparedText.length === 0 || preparedPizza.includes(preparedText)) {
-//                 return pizza;
-//             }
-//         });
-//     };
-
-//     const generateId = () => {
-//         let maxId = 0;
-
-//         items.products.forEach((item) => {
-//             if (item.id > maxId) {
-//                 maxId = item.id;
-//             }
-//         });
-
-//         return maxId + 1;
-//     };
-
-//     const createSchema = (values) => {
-//         return {
-//             id: values.id ?? generateId(),
-//             name: values.name ?? "",
-//             description: values.description ?? "",
-//             image: values.image ?? "/images/home/products/img0001.jpg",
-//             stock: Number(values.stock) ?? 0,
-//             price: Number(values.price) ?? 0,
-//             isPromotion: values.isPromotion ?? false,
-//             isFreeShip: values.isFreeShip ?? false,
-//             category: values.category ?? "",
-//             brand: values.brand ?? "",
-//             type: values.type ?? "",
-//         };
-//     };
-
-//     const createProduct = (values) => {
-//         setItem("products", [ ...items.products, createSchema(values) ]);
-//     };
-
-//     const updateProduct = (values) => {
-//         const index = items.products.findIndex((item) => item.id === values.id);
-//         const products = [...items.products];
-//         products[index] = createSchema(values);
-//         setItem("products", products);
-//     };
-
-//     const removeProduct = (id) => {
-//         const productsWithoutThisProduct = items.products.filter((item) => item.id !== id);
-//         setItem("products", productsWithoutThisProduct);
-//         removeProductFromCart(id);
-//     };
-
-//     return {
-//         products: items.products,
-//         searchProducts,
-//         createProduct,
-//         updateProduct,
-//         removeProduct,
-//     };
-// };
-
-// export default useProducts;
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useProducts = () => {
     const [ products, setProducts ] = useState([]);
-    // const [ response, setResponse ] = useState(null);
-    const [ error, setError ] = useState(null); // Agregué el estado de error para manejar errores
+    const [ error, setError ] = useState(null);
 
     const backendUrl = "https://mitienda-juan.onrender.com/api/products/";
 
@@ -104,7 +14,7 @@ const useProducts = () => {
                 setProducts(response.data.data);
             } catch (error) {
                 console.error("Error fetching products:", error);
-                setError(error); // Setear el estado de error si ocurre un error al obtener los productos
+                setError(error);
             }
         };
 
@@ -117,7 +27,7 @@ const useProducts = () => {
             return response.data.data;
         } catch (error) {
             console.error("Error searching products:", error);
-            throw error; // Relanzar el error para que el componente que llama a esta función pueda manejarlo
+            throw error;
         }
     };
 
@@ -136,21 +46,8 @@ const useProducts = () => {
         }
     };
 
-    // const updateProduct = async (values) => {
-    //     try {
-    //         console.log("Values to update:", values); // Agregar este console.log
-    //         const updatedValues = { ...values }; // Copiar todos los valores de 'values' a 'updatedValues'
-    //         delete updatedValues._id; // Eliminar el campo _id de 'updatedValues'
-    //         const response = await axios.put(`https://mitienda-juan.onrender.com/api/products/${values.id}`, updatedValues);
-    //         setProducts(products.map((product) => (product.id === values ? response.data.data : product)));
-    //     } catch (error) {
-    //         console.error("Error updating product:", error);
-    //     }
-    // };
-
     const updateProduct = async (values) => {
         try {
-            // Desestructurar id y _id sin asignarlos a ninguna variable
             const { _id, id, ...updatedValues } = values;
 
             const response = await axios.put(`${backendUrl}/${values.id}`, updatedValues);
@@ -178,7 +75,6 @@ const useProducts = () => {
 
     return {
         products,
-        // response,
         error,
         searchProducts,
         createProduct,
